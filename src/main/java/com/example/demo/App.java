ce98b1021c315c2b952ee3e8687993846b97da27
Sangmeeeee <goodman1223@naver.com>
2022-02-02 19:25:52 +0900

    Update App.java

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -16,7 +16,7 @@ public class App {
 
 	@RequestMapping("/")
     public String home() {
-        return "Hello world3.";
+        return "Hello world.";
     }
 
     public static void main(String[] args) {
